37050f52a560d25c186ad159e8e52e6dd7230d77
Sadeep Madurange <sadeep+demo@quix.io>
2023-10-20 07:24:27 +0000

    Edited 'main.py'

--- ICE-test-input-generator/main.py
+++ ICE-test-input-generator/main.py
@@ -2,14 +2,25 @@ import quixstreams as qx
 import time
 import datetime
 import os
-import random   
+import random
+
 client = qx.QuixStreamingClient()
 topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
 stream = topic_producer.create_stream() 
-while True:
-    throttle_angle = random.uniform(8, 12)
+
+#while True:
+#    throttle_angle = random.uniform(8, 12)
+#    data = qx.TimeseriesData()
+#    data.add_timestamp(datetime.datetime.utcnow()) \
+#        .add_value("throttle_angle", throttle_angle)
+#    stream.timeseries.publish(data)
+#    time.sleep(1)
+
+for i in range(30):
     data = qx.TimeseriesData()
     data.add_timestamp(datetime.datetime.utcnow()) \
-        .add_value("throttle_angle", throttle_angle)
+        .add_value("throttle_angle", i)
     stream.timeseries.publish(data)
     time.sleep(1)
+
+print("Done")
